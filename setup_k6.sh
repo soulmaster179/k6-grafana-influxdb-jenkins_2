844387b9a147c740103c5d5a5e558c9076d0247c
soulmaster179 <soulmaster179@gmail.com>
2023-02-16 17:03:13 +0700

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -4,7 +4,8 @@ set -ex
 if ! k6 version &> /dev/null
 then
   echo "[setup] installing k6 ..."
-  gpg --keyserver-options no-use-temp-files --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C780D0BDB1A69C86
+  //gpg --keyserver-options no-use-temp-files --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C780D0BDB1A69C86
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C780D0BDB1A69C86
   echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | tee /etc/apt/sources.list.d/k6.list
   apt-get update
   apt-get install k6
